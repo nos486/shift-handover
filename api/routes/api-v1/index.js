71f06c9a524eb7270e4b527f5ba105af7471aa80
sina <nos486@gmail.com>
2022-05-05 03:32:11 +0430

    update

--- api/routes/api-v1/index.js
+++ api/routes/api-v1/index.js
@@ -1,9 +1,13 @@
 const express =require( "express");
 const user =require( "./user");
+const event =require( "./event");
+const domain =require( "./domain");
 
 
 const router = express.Router();
 router.use("/user",user)
+router.use("/event",event)
+router.use("/domain",domain)
 
 
 module.exports = router
