1fe46201aeed5c60abca9ebac88fd02ed72dc04b
sina <nos486@gmail.com>
2022-05-22 21:03:24 +0430

    add

---
+++ front/plugins/axios.js
@@ -0,0 +1,29 @@
+import appConfig from "../config";
+
+export default function ({$axios, store, route, app}) {
+  // $axios.defaults.withCredentials = true;
+  $axios.interceptors.request.use((config) => {
+    config.baseURL = appConfig.apiUrl
+    if (store.getters["services/user/jwtToken"] !== "") {
+      config.headers["Authorization"] = `Bearer ${store.getters["services/user/jwtToken"]}`
+    }
+    return config
+  })
+
+  $axios.interceptors.response.use((response) => {
+    return response;
+  }, (error) => {
+    if ((error.request.config === undefined || error.request.config.alert) && error.response && error.response.data) {
+      if (error.response.status === 401) {
+        app.router.push("/login")
+      } else {
+        if (error.config.alert !== false || error.config.alert === undefined) {
+          console.log(error.config.alert,error.config.alert)
+          store.commit("alert/error", error.response.data.message)
+        }
+        return Promise.reject(error.response.data);
+      }
+    }
+    return Promise.reject(error);
+  })
+}
