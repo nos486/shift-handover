f527105d8003d0a8af01b2df94aaff142a174a2e
sina <nos486@gmail.com>
2022-05-22 21:00:55 +0430

    add events

---
+++ front/pages/events/_id.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="d-flex flex-column">
+    {{data}}
+  </div>
+</template>
+
+<script>
+import menu from "@/menu";
+export default {
+  data : ()=>{
+    return {
+      menu: menu.getItem("event"),
+      data : {}
+    }
+  },
+  mounted() {
+    this.id = this.$route.params.id
+    this.getData()
+  },
+  methods :{
+    getData() {
+      this.$store.dispatch("services/global/get",{
+        pathName : this.menu.value,
+        header : {
+          _id : this.id
+        }
+      }).then((res)=>{
+        this.data = res
+      })
+    }
+  }
+}
+
+</script>
