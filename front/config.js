240764313868765ec5e715c430741691c58094dd
sina <nos486@gmail.com>
2022-06-14 21:28:07 +0430

    update

--- front/config.js
+++ front/config.js
@@ -1,4 +1,5 @@
 export default {
   // apiUrl: "http://192.168.128.15:3000/api/v1/",
-  apiUrl: "http://0.0.0.0:3000/api/v1/"
+  // apiUrl: "http://0.0.0.0:3000/api/v1/"
+  apiUrl: "http://45.149.78.137:3000/api/v1/"
 }
