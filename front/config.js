4494cc550e293ca56fef8b017e5bc8129bc557b9
sina <nos486@gmail.com>
2022-07-08 03:28:43 +0430

    update

--- front/config.js
+++ front/config.js
@@ -1,5 +1,5 @@
 export default {
   // apiUrl: "http://192.168.128.15:3000/api/v1/",
   // apiUrl: "http://0.0.0.0:3000/api/v1/"
-  apiUrl: "http://156.253.5.25:3000/api/v1/"
+  apiUrl: "https://handover.ai6.ir/api/v1/"
 }
