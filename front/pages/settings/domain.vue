f6e9797606bd61147b4e834b254b4066cdd39fbb
sina <nos486@gmail.com>
2022-10-31 06:54:06 +0330

    update

--- front/pages/settings/domain.vue
+++ front/pages/settings/domain.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <APITable :menu="menu">
-
+      <template slot="item.eventsList" slot-scope="{item}">
+        <v-chip v-for="event in item.eventsList" class="ma-1" small :color="$store.getters['app/baseColor']" dark>
+          {{event}}
+        </v-chip>
+      </template>
     </APITable>
   </div>
 </template>
